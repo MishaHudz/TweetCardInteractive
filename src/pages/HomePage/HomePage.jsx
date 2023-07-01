58e34b59be193fbac56ad872da39bd6684d172ee
MishaHudz <misha.hudz@gmail.com>
2023-07-02 00:16:44 +0300

    add home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,36 @@
-import { HomePageSection } from './HomePage.styled';
-
+import {
+  HomePageDescription,
+  HomePageHeader,
+  HomePageSection,
+  LeftImg,
+  LeftSideContainer,
+  TweetsLink,
+} from './HomePage.styled';
+import TopTweetImg from '../../images/top-tweet-card-img.png';
 function HomePage() {
   return (
     <HomePageSection>
-      <h1>Home page</h1>
+      <LeftSideContainer>
+        <LeftImg
+          src={TopTweetImg}
+          alt="question mark and check mark"
+          width="308"
+          height="168"
+        />
+      </LeftSideContainer>
+
+      <div>
+        <HomePageHeader>
+          Explore <br /> Tweets
+        </HomePageHeader>
+        <HomePageDescription>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi eius
+          minus consectetur dolores cum. Obcaecati, voluptatem voluptates rerum
+          ut velit, veniam eos temporibus pariatur aliquam, nobis aliquid ea
+          voluptate? Labore?
+        </HomePageDescription>
+        <TweetsLink to="/tweets">Tweets</TweetsLink>
+      </div>
     </HomePageSection>
   );
 }
